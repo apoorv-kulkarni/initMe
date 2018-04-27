4659f72932817d830d0c46b5065b367d087d8f76
Apoorv Kulkarni <apoorvk009@gmail.com>
2018-04-26 20:32:42 -0700

    necessary now
    improved initial version of shellscript thing

---
+++ initThis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###Get Brew first
+$ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+cp .bash_profile ~/.bash_profile && source ~/.bash_profile
+
+## Necessary brew things that I think are needed
+brew install git;
+brew install gpg2;
+brew install node;
+easy_install pip;
+
+### Git config, purposefully commented out
+#git config --global user.name "FN LN"
+#git config --global user.email "<email@email.com>"
+#git config --global user.signingkey <id>
+
+
+# Feed this do while loop a list of vscode extensions to install on vs code; 
+# no sync nonsense because security is paramount
+
+# note to self: before you migrate to a different machine, 
+# please `run code --list-extensions` on old device's terminal 
+# and then update this file vscode-extensions-list.txt if necessary
+
+while read p; do code --install-extension $p; done <vscode-extensions-list.txt 
