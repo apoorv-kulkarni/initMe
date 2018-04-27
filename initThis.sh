524181cb56a2d7cf912b8220b5743ede47b75066
Apoorv Kulkarni <apoorvk009@gmail.com>
2018-04-26 21:47:54 -0700

    cleanup
    nice and tidy

--- initThis.sh
+++ initThis.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-#install Command Line Tools in Mac OS X
+# Install Command Line Tools in Mac OS X
+echo "Installing Command Line Tools"
 xcode-select --install;
 
-###Get Brew first
+# Get Brew first
 if ! command -v brew >/dev/null 2>&1; then
     echo "You may be asked for your sudo password to install Homebrew:"
     sudo -v
     yes '' | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# force copy bash profile
-echo "copying the bash profile"
+# Force copy bash profile
+echo "Copying the bash profile"
 mv .bash_profile ~/.bash_profile && source ~/.bash_profile;
 
 
-############ Install cask ############
+# Install cask 
 brew tap caskroom/cask
 
 brew_apps=(
@@ -36,21 +37,23 @@ apps=(
   visual-studio-code
 )
 
-## Necessary brew things that I think are needed
+# Necessary brew things that I think are needed
 # Install apps to /Applications
 # Default is /Users/$user/Applications
-echo "installing brew apps for you..."
+echo "Installing brew apps for you"
 brew install  ${brew_apps[@]}
-echo "installing apps like slack, spotify, vscode, chrome, ff, evernote, vlc, iterm2, skype for you..."
+echo "Installing apps like slack, spotify, vscode, chrome, ff, evernote, vlc, iterm2, skype for you"
 brew cask install --appdir="/Applications" ${apps[@]}
 
-# brew cleanup 
+# Brew cleanup 
+echo "Performing a brew cleanup"
 brew cleanup
 
-# for some reason this needs to be installed
+# For some reason this needs to be installed
+echo "Installing pip"
 easy_install pip;
 
-##Git config, purposefully commented out
+# Git config, purposefully commented out
 #git config --global user.name "FN LN"
 #git config --global user.email "<email@email.com>"
 #git config --global user.signingkey <id>
@@ -62,5 +65,5 @@ easy_install pip;
 # note to self: before you migrate to a different machine, 
 # please `run code --list-extensions` on old device's terminal 
 # and then update this file vscode-extensions-list.txt if necessary
-
+echo "Installing all these vscode extensions that you like"
 while read p; do code --install-extension $p; done <vscode-extensions-list.txt 
